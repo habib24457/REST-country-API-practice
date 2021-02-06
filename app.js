c779f2c65bf3dd763f27ae7a7ba924ccd8cd1808
Habibur Rahman <rahmanhabibur24457@gmail.com>
2021-02-06 23:40:48 +0100

    final code

--- app.js
+++ app.js
@@ -34,5 +34,19 @@ countryDiv.appendChild(eachDiv);
 }
 
 const displayCountryDetail = name =>{
-console.log(name);
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+//console.log(url);
+fetch(url)
+.then(res => res.json())
+.then(data =>renderCountryInfo(data[0])); //in the country api, country name is in 0 index, that's why data[0]
+}
+
+const renderCountryInfo = country =>{
+    const countryDiv = document.getElementById("countryDetails");
+    countryDiv.innerHTML = `
+    <h1>${country.name}</h1>
+    <p>Population:${country.population}</p>
+    <p>Area:${country.area}</p>
+    <img src = "${country.flag}">
+    `
 }
