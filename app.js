046bbfca29337af0629e3a8496e227d9701f6e1a
Habibur Rahman <rahmanhabibur24457@gmail.com>
2021-02-06 01:18:09 +0100

    added api json codes

--- app.js
+++ app.js
@@ -4,22 +4,30 @@ fetch('https://restcountries.eu/rest/v2/all')
 
 const displayCountries = countries =>{
 //console.log(countries);
-const countryDiv = document.getElementById('country-name');
+const countryDiv = document.getElementById('countries');
 for(let i =0;i<countries.length;i++){
 const country = countries[i];
 //console.log(country.name);
 const eachDiv = document.createElement('div');
+eachDiv.className='countryCSS';
 
-const h3 = document.createElement('h3');
-h3.innerText = country.name;
+// const h3 = document.createElement('h3');
+// h3.innerText = country.name;
 
-const countryCapital = document.createElement('p');
-countryCapital.innerText = country.capital;
+// const countryCapital = document.createElement('p');
+// countryCapital.innerText = country.capital;
 
-eachDiv.appendChild(h3);
-eachDiv.appendChild(countryCapital);
+// eachDiv.appendChild(h3);
+// eachDiv.appendChild(countryCapital);
+// countryDiv.appendChild(eachDiv);
+
+//another way of showing the data
+const countryInfo = `
+<h3 class="country-name">${country.name}</h3>
+<p>${country.capital}</p>
+`
+eachDiv.innerHTML = countryInfo;
 countryDiv.appendChild(eachDiv);
 
 }
-
 }
